255b19daacad4a3aebdcafa39410c9b9f671cf29
TiNiZa16388 <tim.zacke@gmx.de>
2025-06-10 23:51:44 +0200

    start of capstone project

---
+++ setup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+export DATABASE_URL="postgres://uft8ln9qccd9q9:pbb3c9bb89ebc34eba2d20e9af7448ab4d9ed2cf6fce6350316191905974b533f@ca8lne8pi75f88.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/dfvh2ss7qh523p"
+export EXCITED="true"
+echo "setup.sh script executed successfully!"
