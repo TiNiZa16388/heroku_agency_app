500637850d45ead2ac7b465b269dce1b9092f910
TiNiZa16388 <tim.zacke@gmx.de>
2025-06-13 15:35:26 +0200

    Commenting out

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ from flask_cors import CORS
 def create_app(test_config=None):
 
     app = Flask(__name__)
-    print('__name__ = ' + __name__)
     setup_db(app)
     CORS(app)
 
@@ -33,34 +32,34 @@ def create_app(test_config=None):
     def get_movies():
         return 'No movies implemented yet.'
     
-    # Implement the endpoint for deletion of actors
-    @app.route("/actors/<int:actor_id>", methods=["DELETE"])
-    def delete_actor(actor_id):
-        return 'No deletion of actors implemented, yet!'
+    # # Implement the endpoint for deletion of actors
+    # @app.route("/actors/<int:actor_id>", methods=["DELETE"])
+    # def delete_actor(actor_id):
+    #     return 'No deletion of actors implemented, yet!'
 
-    # Implement the endpoint for deletion of movies
-    @app.route("/movies/<int:movie_id>", methods=["DELETE"])
-    def delete_movie(actor_id):
-        return 'No deletion of movies implemented, yet!'
+    # # Implement the endpoint for deletion of movies
+    # @app.route("/movies/<int:movie_id>", methods=["DELETE"])
+    # def delete_movie(actor_id):
+    #     return 'No deletion of movies implemented, yet!'
     
-    # Implement the endpoint for movie posting
-    @app.route("/movies", methods=["POST"])
-    def post_movie():
-        return 'no posting implemented!'
+    # # Implement the endpoint for movie posting
+    # @app.route("/movies", methods=["POST"])
+    # def post_movie():
+    #     return 'no posting implemented!'
 
-    # Implement the endpoint for actor posting
-    @app.route("/actor", methods=["POST"])
-    def post_actor():
-        return 'no posting implemented!'
+    # # Implement the endpoint for actor posting
+    # @app.route("/actors", methods=["POST"])
+    # def post_actor():
+    #     return 'no posting implemented!'
     
-    # Implement the endpoint for the actor patching
-    @app.route('/actors/<int:actor_id>',methods = ["PATCH"])
-    def patch_actor(actor_id):
-        return 'No Patching implmented'
+    # # Implement the endpoint for the actor patching
+    # @app.route('/actors/<int:actor_id>',methods = ["PATCH"])
+    # def patch_actor(actor_id):
+    #     return 'No Patching implmented'
     
-    @app.route('/movies/<movie_id>', methods=["PATCH"])
-    def patch_movie(movie_id):
-        return 'No Patching of movies implmented'
+    # @app.route('/movies/<movie_id>', methods=["PATCH"])
+    # def patch_movie(movie_id):
+    #     return 'No Patching of movies implmented'
 
 
 app = create_app()
