922f11ca96404655691377978932d6c4c27239a4
TiNiZa16388 <tim.zacke@gmx.de>
2025-07-08 14:00:28 +0200

    Adding environment data for Authentication

--- setup.sh
+++ setup.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 export DATABASE_URL="postgres://uft8ln9qccd9q9:pbb3c9bb89ebc34eba2d20e9af7448ab4d9ed2cf6fce6350316191905974b533f@ca8lne8pi75f88.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/dfvh2ss7qh523p"
 export EXCITED="true"
+
+export AUTH0_DOMAIN="dev-mzcsm1srpeytwi51.eu.auth0.com"
+export AUTH0_CLIENT_ID="5M60sWQqPYOwpodl4LtnZ30UKHtBiHM5"
+export AUTH0_SECRET="NKznfDyMUHjiZcZkT70VHOtdfBisJKrllAVc0vNUsmOyUVsdYGeMn-usnUp-GYy3"
+
 echo "setup.sh script executed successfully!"
